be2ce40a80373e9eb4d283a7aa2ceb6a7e0afafd
Mousume Akther <126440375+Mousumebd@users.noreply.github.com>
2023-04-18 13:11:05 +0600

    Add files via upload

---
+++ mouthree/js/main.js
@@ -0,0 +1,11 @@
+//  Bx Slider
+$(document).ready(function () {
+   
+  $('.slider_area').bxSlider();
+  $('#main-menu').meanmenu({
+    meanMenuContainer: '#mobile_menu',
+    meanScreenWidth:992,
+    
+    });
+  
+  });
